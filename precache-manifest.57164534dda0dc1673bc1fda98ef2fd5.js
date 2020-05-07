f3eac194b7bb1c123964ff4d0ba2cf6c50399be1
James Childress <james.childress@zapier.com>
2020-05-07 07:50:24 -0700

    Updates

---
+++ precache-manifest.57164534dda0dc1673bc1fda98ef2fd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c11bb06cd351b22ece021d16dda5f31",
+    "url": "/quill-drag-and-drop-demo/index.html"
+  },
+  {
+    "revision": "71951c8b91e021ad97cf",
+    "url": "/quill-drag-and-drop-demo/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "e567a2155868a9ee4c29",
+    "url": "/quill-drag-and-drop-demo/static/css/main.f42a3b06.chunk.css"
+  },
+  {
+    "revision": "71951c8b91e021ad97cf",
+    "url": "/quill-drag-and-drop-demo/static/js/2.21948dfb.chunk.js"
+  },
+  {
+    "revision": "6e01e0a96923bb54cd98d93fa224e023",
+    "url": "/quill-drag-and-drop-demo/static/js/2.21948dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e567a2155868a9ee4c29",
+    "url": "/quill-drag-and-drop-demo/static/js/main.771cead4.chunk.js"
+  },
+  {
+    "revision": "1588d765f4c3d417ca06",
+    "url": "/quill-drag-and-drop-demo/static/js/runtime-main.7c14beea.js"
+  }
+]);
